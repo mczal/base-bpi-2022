1a9a0c03f6432a150e875eda49333ddf4ae5a557
Rizal <mseptrianto@wellcode.io>
2023-01-23 21:51:10 +0700

    Deployment preparation

--- scripts/reset-development.sh
+++ scripts/reset-development.sh
@@ -7,8 +7,8 @@ bundle exec rails banks:seed:populate
 bundle exec rails rates:seed:bank_of_indonesia
 bundle exec rails rates:seed:ministry_of_finance
 
-bundle exec rails rates:scrap:bank_of_indonesia
-bundle exec rails rates:scrap:ministry_of_finance
+xvfb-run -a bundle exec rails rates:scrap:bank_of_indonesia
+xvfb-run -a bundle exec rails rates:scrap:ministry_of_finance
 
 bundle exec rails approval_configuration:seed
 
