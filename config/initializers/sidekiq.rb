204d48e5117d605d3580cff6ac584771e4813768
Rizal <mseptrianto@wellcode.io>
2023-01-24 22:39:45 +0700

    Scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,9 @@
 ENV['REDIS_URL'] ||= 'redis://localhost:6379'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}/1", id: nil }
+  config.redis = { url: "#{ENV['REDIS_URL']}/2", id: nil }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "#{ENV['REDIS_URL']}/1", id: nil }
+  config.redis = { url: "#{ENV['REDIS_URL']}/2", id: nil }
 end
-
