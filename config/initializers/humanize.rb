fbcf4bad82548ef9c97929a37d3a0e48ed889c87
Rizal <mseptrianto@wellcode.io>
2023-06-08 07:43:31 +0700

    Humanize configuration

---
+++ config/initializers/humanize.rb
@@ -0,0 +1,5 @@
+Humanize.configure do |config|
+  config.default_locale = :id # [:en, :es, :fr, :tr, :de, :id], default: :en
+  config.decimals_as = :digits # [:digits, :number], default: :digits
+end
+
